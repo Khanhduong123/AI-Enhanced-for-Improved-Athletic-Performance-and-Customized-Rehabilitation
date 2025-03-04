2494929e3421909fd884a058db3df50f3bfc4a8e
Min_coding <hathenhanfpt@gmail.com>
2025-03-04 10:39:01 +0700

    NhanHT/replace structure of project and update the structure of backend

---
+++ backend_capstone/main.py
@@ -0,0 +1,15 @@
+import os
+import uvicorn
+from src import api_v1_router
+from src.create_app import create_app
+# Create FastAPI app instance
+app = create_app()
+
+
+# Add routes
+app.include_router(api_v1_router, prefix="/api")
+
+
+# Launch FastAPI app
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=os.environ.get("PORT", 7860))
